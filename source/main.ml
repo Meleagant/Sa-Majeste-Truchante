a0b62b5d26ab04cdf0d1e9b255616f0866078f12
David Reboullet <reboulle@clipper.ens.fr>
2017-05-22 13:43:55 +0200

    Change un peu la structure du main

--- source/main.ml
+++ source/main.ml
@@ -1,41 +1,43 @@
 
-	(*******)
-	(* SAT *)
-	(*******)
+module Main (S : Sat.Type) = struct
 
-module S : Sat.Type = Sat_epate
+		(*******)
+		(* SAT *)
+		(*******)
 
-let load_sat filename =
-
-	let file = open_in filename in
-	let lexbuf = Lexing.from_channel file in
-	let instance = Parser.sat_file Lexer.token lexbuf in
-	let () = close_in file in
-
-	instance
-
-let run_sat instance =
-
-	match S.resolve instance with
-	| Sat.SAT _ -> Format.printf "SAT@."
-	| Sat.UNSAT -> Format.printf "UNSAT@."
-
-	(*******)
-	(* SMT *)
-	(*******)
-
-module SMT (T : Theory.Type) = struct
-
-	module I = Smt.Make (S) (T)
-
-	let load_smt filename =
+	let load_sat filename =
 
 		let file = open_in filename in
 		let lexbuf = Lexing.from_channel file in
-		let instance = Parser.file Lexer.token lexbuf in
+		let instance = Parser.sat_file Lexer.token lexbuf in
 		let () = close_in file in
 
 		instance
+
+	let run_sat instance =
+
+		match S.resolve instance with
+		| Sat.SAT _ -> Format.printf "SAT@."
+		| Sat.UNSAT -> Format.printf "UNSAT@."
+
+		(*******)
+		(* SMT *)
+		(*******)
+
+	module SMT (T : Theory.Type) = struct
+
+		module I = Smt.Make (S) (T)
+
+		let load_smt filename =
+
+			let file = open_in filename in
+			let lexbuf = Lexing.from_channel file in
+			let instance = Parser.file Lexer.token lexbuf in
+			let () = close_in file in
+
+			instance
+
+	end
 
 end
 
@@ -64,12 +66,14 @@ end
 	(* main *)
 	(********)
 
+module MAIN = Main(Sat_epate)
+
 let () = begin
 
 	match Array.to_list Sys.argv with
 	| _ :: "--sat" :: filename :: _ ->
-		let i = load_sat filename in
-		run_sat i
+		let i = MAIN.load_sat filename in
+		MAIN.run_sat i
 	| _ :: "--test" :: _ ->
 		run_tests ()
 	| _ -> Format.printf "Wrong usage@."
